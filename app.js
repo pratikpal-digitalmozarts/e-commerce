613ed72418004104723a877f6c2440363ea8911c
pratikpal-digitalmozarts <97144487+pratikpal-digitalmozarts@users.noreply.github.com>
2023-08-17 22:56:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.get("/contact", (req, res) => {
 })
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
  console.log(`Server is running on port ${PORT}`);
 });
